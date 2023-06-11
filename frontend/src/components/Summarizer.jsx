070e2dce663f6a4b4f4faf47f62ee07d5dfd59eb
GPV <gvill005@gmail.com>
2023-06-11 15:33:42 -0700

    finished funcitonalities

--- frontend/src/components/Summarizer.jsx
+++ frontend/src/components/Summarizer.jsx
@@ -1,7 +1,8 @@
 import { Container, Card } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
-// import { copy, loader, tick, linkIcon } from '../assets';
+import { copy, loader, tick, linkIcon } from '../assets';
 import { Form, Button, InputGroup} from 'react-bootstrap';
+import { useLazyGetSummaryQuery } from '../slices/articleApi';
 
 const Summarizer = () => {
 
@@ -10,32 +11,53 @@ const Summarizer = () => {
     summary: "",
   });
 
+  //  after setting new article, push it to all articles array.
+  const [allarticles, setAllArticles] = useState([]);
+
+  const [ copied, setCopied ] = useState("");
+
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  // useEffect hook is used to store articles in local storage
   useEffect(() => {
-    // GET ARTICLES FROM LOCAL STORAGE
-  })
+    // get data and store in variable
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem('articles')
+    )
+    console.log("articlesfromSTORAGE", articlesFromLocalStorage)
+    if(articlesFromLocalStorage){
+      setAllArticles(articlesFromLocalStorage)
+    }
+  }, []); // empty dep array to execute hook as soon as page loads
 
   // API request to GPT based summarizer
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("submit")
-    // const existingArticle = allArticles.find(
-    //   (item) => item.url === article.url
-    // );
+    const { data } = await getSummary({ articleUrl: article.url });
 
-    // if (existingArticle) return setArticle(existingArticle);
+    if(data?.summary){
+      const newArticle = { ...article, summary: data.summary };
+      
+      const updatedAllArticles = [newArticle, ...allarticles];
 
-    // const { data } = await getSummary({ articleUrl: article.url });
-    // if (data?.summary) {
-    //   const newArticle = { ...article, summary: data.summary };
-    //   const updatedAllArticles = [newArticle, ...allArticles];
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
 
-    //   // update state and local storage
-    //   setArticle(newArticle);
-    //   setAllArticles(updatedAllArticles);
-    //   localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
-    // }
+      // update local storage
+      localStorage.setItem('articles', JSON.stringify(updatedAllArticles))
+
+      console.log("newArticle", newArticle)
+    }
   };
 
+  // autocopy icon functionality
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    // built in and makes copy button work
+    navigator.clipboard.writeText(copyUrl);
+    // show successful animation
+    setTimeout(() => setCopied(false), 3000)
+  }
   return (
     <div className='py-5'>
       <Container className='d-flex justify-content-center'>
@@ -53,6 +75,7 @@ const Summarizer = () => {
               
               <InputGroup className="d-flex align-items-center">
               <Form.Label>Input</Form.Label>
+              
               <Form.Control
                 type='url'
                 placeholder='Paste Article Link'
@@ -64,14 +87,62 @@ const Summarizer = () => {
               ↵
               </Button>
               </InputGroup>
+
             </Form.Group>
           </Form>
 
           {/* BROWSER URL HISTORY */}
 
-          </Card>
+          <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+            { allarticles.map((item, index) => (
+              <div
+                key={`link-${index}`}
+                onClick={() => setArticle(item)} 
+                className="link_card"
+              >
+                <div className="copy_btn" onClick={() => handleCopy(item.url)}>
+                  <img 
+                    src={copied === item.url ? tick : copy} 
+                    className='w-[40%] h-[40%] object-contain'
+                    alt='copy_icon'
+                  />
+                </div>
+                <p className='flex-1 fot-satoshi text-blue-700 font-medium text-sm truncate'>
+                  {item.url}
+                </p>
+              </div>
+            ))}
+          </div>
 
           {/* Display Results */}
+          <div className='my-10 max-w-full flex justify-center items-center'>
+            {isFetching ? (
+              <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
+                ) : error ? (
+              <p className='font-inter font-bold text-black text-center'>
+                Well, that wasn't supposed to happen...
+                <br />
+                <span className='font-satoshi font-normal text-gray-700'>
+                  {error?.data?.error}
+                </span>
+              </p>
+              ) : (
+              article.summary && (
+                <div className='flex flex-col gap-3'>
+                  <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+                    Article <span className='blue_gradient'>Summary</span>
+                  </h2>
+                  <div className='summary_box'>
+                    <p className='font-inter font-medium text-sm text-gray-700'>
+                      {article.summary}
+                    </p>
+                  </div>
+                </div>
+              )
+            )}
+          </div>
+
+          </Card>
         </Container>
 
     </div>
--- frontend/src/slices/articleApi.js
+++ frontend/src/slices/articleApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const rapidApiKey = process.env.X_RAPIDAPI_KEY;
+const rapidApiKey = 'a269033e8emsh9ca0122b4dde6ccp1f154bjsn5f724c1f12b9';
 
 export const articleApi = createApi({
     reducerPath: 'articleApi',
@@ -17,7 +17,7 @@ export const articleApi = createApi({
         getSummary: builder.query({
             // encodeURIComponent() function encodes special characters that may be present in the parameter values
             // If we do not properly encode these characters, they can be misinterpreted by the server and cause errors or unexpected behavior. Thus that RTK bug
-            query: (params) => `summarize?url=${encodeURIComponent(params.articleUrl)}&length=10`,
+            query: (params) => `summarize?url=${encodeURIComponent(params.articleUrl)}&length=1`,
         }),
     }),
 })
