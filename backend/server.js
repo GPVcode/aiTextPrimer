6db850bbd25ba8ef3bac5f7c36d2808b9865a4bd
GPV <gvill005@gmail.com>
2023-06-17 13:30:48 -0700

    brought back dir code in server

--- backend/server.js
+++ backend/server.js
@@ -24,17 +24,17 @@ app.use('/api/users', userRoutes);
 app.use(cors());
 
 // handle deployment
-// if(process.env.NODE_ENV === 'production'){
-//     const __dirname = path.resolve();
+if(process.env.NODE_ENV === 'production'){
+    const __dirname = path.resolve();
     
-//     app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-//     app.get('*', (req, res) => 
-//         res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))
-//     );
-// } else {
-//     app.get('/', (req, res) => res.send('Server is ready'));
-// }
+    app.get('*', (req, res) => 
+        res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))
+    );
+} else {
+    app.get('/', (req, res) => res.send('Server is ready'));
+}
 
 // custom error middleware
 app.use(notFound);
