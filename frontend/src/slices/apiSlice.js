b70734ad6a2508eaface9d93a0126a79a745adb9
GPV <gvill005@gmail.com>
2023-06-17 13:44:48 -0700

    found bug

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,6 +1,6 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: 'https://ai--primer.onrender.com/' });
+const baseQuery = fetchBaseQuery({ baseUrl: 'https://ai-blog-primer.onrender.com/' });
 
 export const apiSlice = createApi({
   baseQuery,
