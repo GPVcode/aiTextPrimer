d734d7409f47e10a1469c90f64ee9c04f10d145d
GPV <gvill005@gmail.com>
2023-06-17 11:45:55 -0700

    updared required for register form

--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -53,10 +53,12 @@ const RegisterScreen = () => {
                 <div>
                     <label for="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
                     <input
+                        name='name'
                         type='name'
                         id='name'
                         className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder='Enter Name'
+                        required
                         value={name}
                         onChange={ (e) => setName(e.target.value) }
                     ></input>
